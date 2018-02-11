7560f9fc26e02019104d3ce81cde1911b7ea6281
Lakshay Lakhani <lakshay@technoarchsoftwares.com>
2018-02-11 18:30:01 +0530

    handles exceptions

--- select_geoname/views.py
+++ select_geoname/views.py
@@ -19,43 +19,49 @@ def where_are_you(request):
 
     if request.is_ajax() and request.GET['action']=="for_states":
         country_id = request.GET['country_id']
-        country = cities.models.Country.objects.get(id=country_id)
-        if country:
-            states = cities.models.Region.objects.filter(country=country)
-            context.update({
-                "states":states,
-            })
-            template = render_to_string("select_geoname/get_states.html", context)
-            response = {"data": template}
-            return HttpResponse(json.dumps(response), content_type="application/json")
+        if country_id:
+            country = cities.models.Country.objects.filter(id=country_id).first()
+            if country:
+                states = cities.models.Region.objects.filter(country=country)
+                context.update({
+                    "states":states,
+                })
+                template = render_to_string("select_geoname/get_states.html", context)
+                response = {"data": template}
+                return HttpResponse(json.dumps(response), content_type="application/json")
 
     if request.is_ajax() and request.GET['action']=="for_cities":
         state_id = request.GET['state_id']
-        state = cities.models.Region.objects.get(id=state_id)
-        if state:
-            citys = cities.models.City.objects.filter(region=state)
-            context.update({
-                "cities":citys,
-            })
-            template = render_to_string("select_geoname/get_cities.html", context)
-            response = {"data": template}
-            return HttpResponse(json.dumps(response), content_type="application/json")
+        if state_id:
+            state = cities.models.Region.objects.filter(id=state_id).first()
+            if state:
+                citys = cities.models.City.objects.filter(region=state)
+                context.update({
+                    "cities":citys,
+                })
+                template = render_to_string("select_geoname/get_cities.html", context)
+                response = {"data": template}
+                return HttpResponse(json.dumps(response), content_type="application/json")
 
     if request.is_ajax() and request.GET['action']=="form_submit":
         form_data = request.GET["form_data"]
         country_id = request.GET["country"]
         state_id = request.GET["state"]
         city_id =  request.GET["city"]
+        data = {}
 
-        city = cities.models.City.objects.filter(id=city_id).first()
-        country = cities.models.Country.objects.filter(id=country_id).first()
-        state = cities.models.Region.objects.filter(id=state_id).first()
+        if city_id:
+            city = cities.models.City.objects.filter(id=city_id).first()
+            data.update({"city_name":city.name})
 
-        data={
-            "city_name":city.name,
-            "country_name":country.name,
-            "state_name":state.name
-        }
+        if state_id:
+            country = cities.models.Country.objects.filter(id=country_id).first()
+            data.update({"country_name":country.name})
+
+        if country_id:
+            state = cities.models.Region.objects.filter(id=state_id).first()
+            data.update({"state_name":state.name})
+
         return JsonResponse(data)
 
     context.update({
